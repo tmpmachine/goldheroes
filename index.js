b7da3b1daa8093d252e8331f8d7c2b5e2a691edc
tmpmachine <bill.yosafatz@gmail.com>
2024-06-25 09:55:46 +0700

    chore: moving files

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-app.Init();
+import { loadScripts } from './script-loader.js';
 
+// load files
 (function() {
 
-  if (typeof(navigator) !== 'undefined' && 'serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js').then(function(swo) {
-      console.info('Service worker registerd successfully');
-    }).catch(function(e) {
-      console.error(e);
-    });
-  }
+  loadScripts([
+    {
+      urls: [
+        "js/utils/data-server.js",
+        "js/utils/list-container-builder.js",
+      ],
+    },
+    {
+      urls: [
+        "js/app.js",
+        "js/app-data.js",
+        "js/dom-events.js",
+        "js/components/inventory-component.js",
+        "js/components/reward-component.js",
+        "js/components/stats-component.js",
+        "js/ui.js",
+        "js/uis/inventory-ui.js",
+      ],
+      callback: function() {
+        app.Init();
+      }
+    },
+  ]);
   
 })();
+
+// register service worker
+if (typeof(navigator) !== 'undefined' && 'serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js').then(function(swo) {
+    console.info('Service worker registerd successfully');
+  }).catch(function(e) {
+    console.error(e);
+  });
+}
